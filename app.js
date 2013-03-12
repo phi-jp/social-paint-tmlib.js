c08a573dbfeca46f55bd947efbb3af09c58762a0
phi <phi1618jp@gmail.com>
2013-03-13 02:18:23 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3001);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+var io = require('socket.io').listen(server);
+//var socket = require('socket.io').listen(app);
+io.sockets.on('connection', function(socket) {
+  socket.on('start', function(data) {
+    socket.emit("start", {
+      userId: socket.handshake.userId,
+      data:data
+    });
+  });
+  socket.on('move', function(data) {
+    socket.broadcast.emit("move", {
+      userId: socket.handshake.userId,
+      data:data
+    });
+  });
+});
---
+++ public/javascripts/main.js
@@ -0,0 +1,71 @@
+
+;(function() {
+    var socket  = null;
+    var canvas  = null;
+    var element = null;
+
+    var initCanvas = function() {
+        canvas = tm.graphics.Canvas("#world");
+        canvas.resize(465, 465);
+        canvas.lineWidth = 8;
+        canvas.setLineStyle(8);
+    };
+
+    var send = function(act, startX, startY, endX, endY, color) {
+        socket.emit(act, {
+            startX:startX,
+            startY:startY,
+            endX:endX,
+            endY:endY,
+            color:color
+        });
+    };
+
+    var initSocketIO = function() {
+        socket  = io.connect('http://'+location.host+':3001/');
+        console.log('http://'+location.host+'/');
+    };
+
+    tm.main(function() {
+        initCanvas();
+        initSocketIO();
+        
+        var element = tm.dom.Element("#world");
+        var x, y;
+        var touch = false;
+        var color = "hsl({0}, 75%, 50%)".format(Math.rand(0, 360));
+
+        element.event.pointstart(function(e) {
+            touch = true;
+            x = e.pointX; y = e.pointY;
+            send("start");
+        });
+        element.event.pointmove(function(e) {
+            if (!touch) { return ; }
+
+            canvas.strokeStyle = color;
+            canvas.drawLine(x, y, e.pointX, e.pointY);
+            send("move", x, y, e.pointX, e.pointY, color);
+            x = e.pointX; y = e.pointY;
+        });
+        element.event.pointend(function() {
+            touch = false;
+            send("end");
+        });
+        
+        socket.on('connect', function() {
+            console.log('Cliant-connect');
+        });
+        
+        socket.on('start', function(data) {
+            console.dir(data);
+        });
+        
+        socket.on('move', function(e) {
+            var d = e.data;
+            canvas.strokeStyle = e.data.color;
+            canvas.drawLine(d.startX, d.startY, d.endX, d.endY);
+        });
+    });
+
+})();
